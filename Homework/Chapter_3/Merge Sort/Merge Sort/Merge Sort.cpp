d8f2b8f2b9fe4bb31ad673edd2dce9239d4aae13
Mediblo <jjssog@naver.com>
2023-04-21 19:54:19 +0900

    Quick Sort 제작 (o@o)//

---
+++ Homework/Chapter_3/Merge Sort/Merge Sort/Merge Sort.cpp
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+
+#define max_size 12
+
+int A[max_size] = { 6, 3, 11, 9, 12, 2, 8, 15, 18, 10, 7, 14};
+
+void merge(int p, int q);
+
+int main() {
+
+	for (int i = 0; i < max_size; i++) printf("%d ", A[i]);
+	printf("\n");
+	printf("==========================\n");
+	merge(0, max_size);
+
+	printf("==========================\n");
+	for (int i = 0; i < max_size; i++) printf("%d ", A[i]);
+	printf("\n");
+	return 0;
+}
+
+void merge(int p, int q) {
+	if (p < q) {
+		int k = (p + q) / 2;
+
+		merge(p, k);
+		merge(k + 1, q);
+		printf("[%d %d %d]\t", p, q, k);
+
+		int ci = 0;
+		int ai = p;
+		int bi = k + 1;		
+		int C[max_size + 1]{};
+
+		while (ci+1 <= q - p + 1)
+		{
+			if (bi > q) bi = p;
+			else if (ci + 1 < q - p + 1 && ai == bi) bi++, printf("[LV] ");
+			if (A[ai] < A[bi]) C[ci++] = A[ai++], printf("[%d %d] ", A[ai - 1], A[bi]);
+			else C[ci++] = A[bi++], printf("(%d %d) ", A[ai], A[bi - 1]);
+		}
+
+		for(int i=0, j=p; i < q - p + 1; i++, j++) A[j] = C[i];
+		for (int i = 0; i < max_size; i++) printf("%d ", A[i]);
+		printf("\n");
+	}
+}
---
+++ Homework/Chapter_3/Quick Sort/Quick Sort/Quick Sort.cpp
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void quick(int left, int right);
+
+#define MAX_SIZE 20
+
+int A[MAX_SIZE];
+
+int main() {
+
+	srand(time(NULL));
+
+	for (int i = 0; i < MAX_SIZE; i++) A[i] = rand() % 100 + 1;
+
+	for (int i = 0; i < MAX_SIZE; i++) printf("%d ", A[i]);
+	printf("\n");
+
+	quick(0, MAX_SIZE);
+
+	for (int i = 0; i < MAX_SIZE; i++) printf("%d ", A[i]);
+	printf("\n");
+
+	return 0;
+}
+
+void quick(int left, int right) {
+	if( left < right ) {
+		int p = (left + right) / 2;
+
+		int temp = A[right - 1];
+		A[right - 1] = A[p];
+		A[p] = temp;
+
+		int i, j = left;
+
+		for (i = left; i < right; i++) {
+			if (A[i] < A[right-1]) { // i번째가 피벗 숫자보다 작을 경우
+				temp = A[j];
+				A[j++] = A[i];
+				A[i] = temp;
+			}
+		}
+
+		if (j <= right - 1) {
+			temp = A[j];
+			A[j] = A[right - 1];
+			A[right - 1] = temp;
+		}
+
+		quick(left, j); // 왼쪽
+		quick(j+1, right); // 오른쪽
+	}
+}
